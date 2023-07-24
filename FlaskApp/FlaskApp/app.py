42c170f192000201c76d6cf575c210e33cbf50fd
PaulaBeckett <pollysparkles@gmail.com>
2023-07-24 20:25:40 +0930

    files moved and added to Flask

--- FlaskApp/FlaskApp/app.py
+++ FlaskApp/FlaskApp/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def viewOne():
-    return render_template('index3.html')
+    return render_template('map_fire.html')
 
 @app.route("/charts")
 def viewTwo():
@@ -14,7 +14,7 @@ def viewTwo():
 
 @app.route("/morecharts")
 def viewThree():
-    return render_template('index3.html')
+    return render_template('linechart.html')
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8000, debug=True)
